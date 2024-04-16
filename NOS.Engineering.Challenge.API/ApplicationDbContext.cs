a1ad75d10246d2effa7e37a8a093c3e3c00ebfea
Douglas Basilio <basilio.douglas@yahoo.com.br>
2024-04-16 16:52:05 -0300

    feat(main): ajuste na task 3, configuração do banco de dados com migrations

---
+++ NOS.Engineering.Challenge.API/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NOS.Engineering.Challenge.Models;
+
+namespace NOS.Engineering.Challenge.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+        {
+        }
+
+        public ApplicationDbContext() : base()
+        {
+        }
+
+        public DbSet<Content> Contents { get; set; }
+    }
+}
--- NOS.Engineering.Challenge.API/Program.cs
+++ NOS.Engineering.Challenge.API/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 using NOS.Engineering.Challenge.API.Extensions;
+using NOS.Engineering.Challenge.Database;
+using System.Data;
 
 var builder = WebApplication.CreateBuilder(args).ConfigureWebHost().RegisterServices();
 
@@ -7,7 +10,30 @@ builder.Logging.AddConsole();
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 26));
+
+var connectionString = builder.Configuration.GetConnectionString("MariaDB");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, serverVersion)
+);
+
+builder.Services.AddMemoryCache();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetService<ApplicationDbContext>();
+
+    if (dbContext != null && dbContext.Database.GetDbConnection().State != ConnectionState.Open)
+    {
+        dbContext.Database.OpenConnection();
+
+        // Run EnsureCreated() to create the database and any pending migrations
+        dbContext.Database.EnsureCreated();
+    }
+}
 
 app.MapControllers();
 app.UseSwagger()
